b8c82975cef45e045ff6240a8a7c77b31b84fe83
gh2o <gh2o@localhost>
2011-09-12 23:24:32 -0700

    to more closely model vm_(de)allocate, use m(un)map

--- emu/vm.c
+++ emu/vm.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
 #include <mach/vm_map.h>
 #include "log.h"
 
@@ -10,14 +12,27 @@ kern_return_t vm_allocate
 	int flags
 )
 {
+	long page_size = sysconf (_SC_PAGESIZE);
+	
+	long number_of_pages = (size + page_size - 1) / page_size;
+	size_t size_rounded = number_of_pages * page_size;
+	
 	if (!flags) // forced location
 	{
 		EMU_ERROR ("forced address not supported");
 		return KERN_FAILURE;
 	}
 	
-	void *mem = malloc (size);
-	if (!mem)
+	void *mem = mmap (
+		NULL,
+		size_rounded,
+		PROT_READ | PROT_WRITE,
+		MAP_PRIVATE | MAP_ANONYMOUS,
+		-1,
+		0
+	);
+	
+	if (mem == MAP_FAILED)
 	{
 		EMU_ERROR ("malloc failed");
 		return KERN_FAILURE;
@@ -34,6 +49,17 @@ kern_return_t vm_deallocate
 	vm_size_t size
 )
 {
-	free ((void *) address);
+	long page_size = sysconf (_SC_PAGESIZE);
+	
+	long first_page = address / page_size;
+	long last_page = (address + size - 1) / page_size;
+	
+	long number_of_pages = last_page - first_page + 1;
+	
+	void *mstart = (void *)(first_page * page_size);
+	size_t msize = number_of_pages * page_size;
+	
+	munmap (mstart, msize);
+	
 	return KERN_SUCCESS;
 }
